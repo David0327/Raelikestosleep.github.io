307662a99a6fc94e212540b56b6b77acce3609b4
David0327 <86380713+David0327@users.noreply.github.com>
2023-05-10 17:50:36 -0500

    Update init.js

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -48,10 +48,25 @@ var init = function (window) {
         function update() {
 
             // TODO 4 : Update the circle's position //
+             // physikz.updatePosition(circles[1]);
+            // physikz.updatePosition(circles[2]);
+            // physikz.updatePosition(circles[3]);
+            // physikz.updatePosition(circles[4]);
+            // physikz.updatePosition(circles[5]);
+            
+            for (var i = 0; i < circles.length; i++) {
+                var eachCircle = circles[i];
+
+                physikz.updatePosition(eachCircle);
+                game.checkCirclePosition(eachCircle)
            
 
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+            //game.checkCirclePosition(circles[1]);
+            //game.checkCirclePosition(circles[2]);
+            // game.checkCirclePosition(circles[3]);
+            // game.checkCirclePosition(circles[4]);
+            // game.checkCirclePosition(circles[5]);
 
             // TODO 9 : Iterate over the array
             for (var i = 0; circles.length > i; i++) {
@@ -83,7 +98,15 @@ var init = function (window) {
             }
 
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-
+             if (0 > circle.x) {
+                circle.x = canvas.width;
+            }
+            if (circle.y > canvas.height) {
+                circle.y = 0;
+            }
+            if (0 > circle.y) {
+                circle.y = canvas.height;
+            }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
